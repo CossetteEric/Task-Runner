bd78cbd62fccdb1330a4d1cc631293570b3d1ae5
CossetteEric <eric.porter@cossette.com>
2016-06-21 22:49:21 -0400

    Wrote more test cases

--- Functions/Start-Task.Tests.ps1
+++ Functions/Start-Task.Tests.ps1
@@ -1,7 +1,7 @@
 InModuleScope Task-Tree {
-    Describe "the Start-Leaf function" {
+    Describe "the Start-Task function" {
         Mock Write-Host {$Script:HostOutput += $Object}
-        It "runs 1 task" {
+        It "runs a task" {
             $Script:HostOutput = ""
             $Task = @{
                 Path = "Write.Hello"
@@ -9,6 +9,44 @@ InModuleScope Task-Tree {
             }
             Start-Task $Task
             $Script:HostOutput | Should Be "Write.Hello`r`nHello"
+        }
+        It "shows a description" {
+            $Script:HostOutput = ""
+            $Task = @{
+                Path = "Write.Hello"
+                Description = "Write `"Hello`""
+                Action = {Write-Host "Hello"}
+            }
+            Start-Task $Task
+            $Script:HostOutput | Should Be "Write.Hello`r`nWrite `"Hello`"`r`nHello"
+        }
+        It "displays known error" {
+            $Script:HostOutput = ""
+            $Task = @{
+                Path = "Throw.Known"
+                Action = {
+                    throw New-Object Exception("Known Error")
+                }
+                Errors = {
+                    Param($Message)
+                    return $Message -eq "Known Error"
+                }
+            }
+            Start-Task $Task
+            $Script:HostOutput | Should Be "Throw.Known`r`nKnown Error`r`n"
+        }
+        It "throws unknown error" {
+            $Task = @{
+                Path = "Throw.Unknown"
+                Action = {
+                    throw New-Object Exception("Unknown Error")
+                }
+                Errors = {
+                    Param($Message)
+                    return $Message -eq "Known Error"
+                }
+            }
+            {Start-Task $Task} | Should Throw
         }
     }
 }
