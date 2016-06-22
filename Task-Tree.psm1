92c64ef0082a01990a9240cdc59892d54b6d3640
CossetteEric <eric.porter@cossette.com>
2016-06-21 21:13:32 -0400

    Renamed from Build-* to Start-*

--- Task-Tree.psm1
+++ Task-Tree.psm1
@@ -4,6 +4,6 @@ $moduleRoot = Split-Path -Parent $MyInvocation.MyCommand.Path
     ? {!($_.ProviderPath.ToLower().Contains(".tests."))} |
     % {. $_.ProviderPath}
 
-Export-ModuleMember -function "Build-Leaf"
-Export-ModuleMember -function "Build-Tree"
+Export-ModuleMember -function "Start-Leaf"
+Export-ModuleMember -function "Start-Tree"
 Export-ModuleMember -function "Verify-Leaf"
